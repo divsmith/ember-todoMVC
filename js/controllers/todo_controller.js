71b2867fce253af25aa550e6b002600e974f48dc
Parker Smith <parker@parkersmith.us>
2014-09-02 02:11:18 +0100

    Handle double clicks, modify view for editing

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,4 +1,12 @@
 App.TodoController = Ember.ObjectController.extend({
+	actions: {
+		editTodo: function() {
+			this.set('isEditing', true);
+		}
+	},
+
+	isEditing: false,
+
 	isCompleted: function(key, value) {
 		var model = this.get('model');
 
