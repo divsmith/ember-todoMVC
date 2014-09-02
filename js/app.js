9415fb3d1e1c1904298711f4b8cd1d64d0ba7dce
Parker Smith <parker@parkersmith.us>
2014-09-02 05:31:47 +0100

    Add localstorage adapter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 window.App = Ember.Application.create();
 
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+//App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.ApplicationAdapter = DS.LSAdapter.extend({
+	namespace: 'todos-emberjs'
+});
 
