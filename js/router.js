15fedf80c4eea3499a19647ed69a7e9642f2e2ad
Parker Smith <parker@parkersmith.us>
2014-09-02 02:32:53 +0100

    Add child routes

--- js/router.js
+++ js/router.js
@@ -1,9 +1,17 @@
 App.Router.map(function() {
-	this.resource('todos', { path: '/' });
+	this.resource('todos', { path: '/' }, function() {
+
+	});
 });
 
 App.TodosRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
+	}
+});
+
+App.TodosIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('todos');
 	}
 });
